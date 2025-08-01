7ea4ddd50928e2eecf17d3d0bb8a3b2f7e688d96
Sameer Khan <muhammadsameer2016@yahoo.com>
2025-08-01 03:16:16 -0700

    initiate working on agent details page

--- src/modules/agents/views/agents-view.tsx
+++ src/modules/agents/views/agents-view.tsx
@@ -9,9 +9,12 @@ import { columns } from '../components/columns';
 import EmptyState from '@/components/generalComponents/emptyState';
 import { useAgentsFilters } from '../hooks/use-agent-filters';
 import DataPagination from '../components/data-pagination';
+import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navi';
 
 const AgentsView = () => {
     const trpc = useTRPC();
+    const router = useRouter();
     const [filters, setFilters] = useAgentsFilters();
     const [open, setOpen] = useState(true);
     const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
@@ -21,7 +24,7 @@ const AgentsView = () => {
         <>
             {/* <div>{JSON.stringify(data, null, 2)}</div> */}
             <div className='flex pb-4 px-4 md:px-8 flex flex-col gap-y-4'>
-                <DataTable columns={columns} data={data.items} />
+                <DataTable columns={columns} data={data.items} onRowClick={(row)=> router.push(`agents/${row.id}`)}/>
                 <DataPagination page={filters.page} totalPages={data.totalPages} onPageChange={(page) => setFilters({ page })} />
                 {data.items.length == 0 && (
                     <EmptyState title='Create your first agent' description='Create an agent to join you meeting. Each agent will follow your instruction and can interact with participants during th call.' />
